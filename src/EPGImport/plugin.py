a7373762e3e40b07fe455ec4fda345de0cbc2e82
talbss <b_boutaleb@hotmail.com>
2015-10-06 13:02:59 +0200

    [EPGImport] revert the typo fix as it is not a typo
    (cherry picked from commit 17b31f00aa6181dd10745c87c962291731535f7c)
    Signed-off-by: Erik Slagter <erik@openpli.org>

--- src/EPGImport/plugin.py
+++ src/EPGImport/plugin.py
@@ -287,7 +287,7 @@ class EPGImportConfig(ConfigListScreen,Screen):
 		self.cfg_runboot_day = getConfigListEntry(_("Consider setting \"Days Profile\""), self.EPG.runboot_day)
 		self.cfg_runboot_restart = getConfigListEntry(_("Skip import on restart GUI"), self.EPG.runboot_restart)
 		self.cfg_showinextensions = getConfigListEntry(_("Show \"EPGImport\" in extensions"), self.EPG.showinextensions)
-		self.cfg_showinmainmenu = getConfigListEntry(_("Show \"EPGImport\" in main menu"), self.EPG.showinmainmenu)
+		self.cfg_showinmainmenu = getConfigListEntry(_("Show \"EPG Importer\" in main menu"), self.EPG.showinmainmenu)
 		self.cfg_longDescDays = getConfigListEntry(_("Load long descriptions up to X days"), self.EPG.longDescDays)
 		self.cfg_parse_autotimer = getConfigListEntry(_("Run AutoTimer after import"), self.EPG.parse_autotimer)
 		self.cfg_clear_oldepg = getConfigListEntry(_("Clearing current EPG before import"), config.plugins.epgimport.clear_oldepg)
